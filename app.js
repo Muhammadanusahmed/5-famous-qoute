e2a09c74db1d03e597db049392afeba9033bc58a
MuammadAnusAhmed <MUHAMMADANUSAHMEDON@gmail.com>
2024-04-15 17:56:49 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+let famous_person = "Albert Einstein";
+let famous_personName = "A person who never made a mistake never tried anything new.";
+let message = `${famous_person} once said, "${famous_personName}"`;
+console.log(message);
---
+++ app.ts
@@ -0,0 +1,5 @@
+let famous_person: string = "Albert Einstein";
+let famous_personName: string = "A person who never made a mistake never tried anything new.";
+let message: string = `${famous_person} once said, "${famous_personName}"`;
+
+console.log(message);
